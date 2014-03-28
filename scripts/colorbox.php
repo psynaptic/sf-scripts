182c5555a6b7ec2fb268fb2cd0677759af61c4e4
Richard Burford <rich@freestylesystems.co.uk>
2014-03-28 02:25:51 +0000

    Added a check for whether the body field contains the string 'colorbox'.

--- scripts/colorbox.php
+++ scripts/colorbox.php
@@ -8,9 +8,12 @@
 $query = 'SELECT status FROM {system} WHERE name = :name';
 $colorbox = db_query($query, array(':name' => 'colorbox'))->fetchField();
 $media_gallery = db_query($query, array(':name' => 'media_gallery'))->fetchField();
+$body_content = db_query('SELECT bundle, entity_id FROM field_data_body WHERE body_value LIKE :value', array(':value' => '%colorbox%'))->fetchField();
+
 $output['colorbox_state'] = $colorbox ? 'enabled' : 'disabled';
 $output['media_gallery_state'] = $media_gallery ? 'enabled' : 'disabled';
 $output['colorbox_style'] = variable_get('colorbox_style');
 $output['media_gallery_style'] = variable_get('media_gallery_colorbox_stylesheet');
-echo implode(',', $output);
+$output['body_colorbox'] = $body_content ? 'yes' : 'no';
 
+echo implode(',', $output);
