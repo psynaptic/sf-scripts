043fc3e22b8fd27c289f7a85ac2f00cc2d1c9e39
Richard Burford <rich@freestylesystems.co.uk>
2014-04-01 20:04:20 +0100

    Added a few more data points to the colorbox audit script.

--- scripts/colorbox.php
+++ scripts/colorbox.php
@@ -10,9 +10,11 @@ $colorbox = db_query($query, array(':name' => 'colorbox'))->fetchField();
 $media_gallery = db_query($query, array(':name' => 'media_gallery'))->fetchField();
 $body_content = db_query('SELECT bundle, entity_id FROM field_data_body WHERE body_value LIKE :value', array(':value' => '%colorbox%'))->fetchField();
 
-$output['colorbox_state'] = $colorbox ? 'enabled' : 'disabled';
-$output['media_gallery_state'] = $media_gallery ? 'enabled' : 'disabled';
+$output['colorbox'] = $colorbox ? 'enabled' : 'disabled';
+$output['media_gallery'] = $media_gallery ? 'enabled' : 'disabled';
 $output['colorbox_style'] = variable_get('colorbox_style');
+$output['colorbox_path'] = variable_get('colorbox_path');
+$output['colorbox_inline'] = variable_get('colorbox_inline');
 $output['media_gallery_style'] = variable_get('media_gallery_colorbox_stylesheet');
 $output['body_colorbox'] = $body_content ? 'yes' : 'no';
 
