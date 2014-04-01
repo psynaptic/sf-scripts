736b4947a7ffa09b889d383cf663f051226ba9de
Richard Burford <rich@freestylesystems.co.uk>
2014-04-01 21:04:05 +0100

    Improved the archive script.

--- scripts/archive-site.php
+++ scripts/archive-site.php
@@ -1,23 +1,27 @@
 <?php
 
-$sitegroup = 'tangle006';
-$env = 'prod';
-$uri = 'projectroot.pfizer.edrupalgardens.com';
+$sitegroup = '';
+$env = '';
+$sites = array(
+);
 $backup_location = '/tmp/dumps';
-$db_name = vget('gardens_db_name');
 $data = date('YmdHi');
 
 // Create the backup directory.
 create_dir($backup_location);
 
-// Archive the themes and files to the backup directory.
-$files_dir = "/var/www/html/${sitegroup}.${env}/docroot/sites/g/files/${db_name}";
-foreach (array('themes', 'files') as $type) {
-  create_archive("${files_dir}/${type}", "${dump_dir}/${uri}.${date}.${type}.gz");
-}
+foreach ($sites as $uri) {
+  $db_name = vget('gardens_db_name');
 
-// Dump the database to the backup directory.
-dump_database($uri, "@${sitegroup}.${env}" "${backup_location}/${uri}.${date}.sql.gz");
+  // Archive the themes and files to the backup directory.
+  $files_dir = "/var/www/html/${sitegroup}.${env}/docroot/sites/g/files/${db_name}";
+  foreach (array('themes', 'files') as $type) {
+    create_archive("${files_dir}/${type}", "${dump_dir}/${uri}.${date}.${type}.gz");
+  }
+
+  // Dump the database to the backup directory.
+  dump_database($uri, "@${sitegroup}.${env}" "${backup_location}/${uri}.${date}.sql.gz");
+}
 
 function create_dir($dirname) {
   if (!file_exists($dirname)) {
@@ -30,23 +34,24 @@ function create_dir($dirname) {
 function vget($name) {
   $value = variable_get($name);
   if (is_null($value)) {
-    return drush_log("Could not find variable $variable", 'error');
+    return drush_log("Could not find variable $name", 'error');
   }
 }
 
 function create_archive($source, $target) {
-  $return = shell_exec("tar czf $target -C $source .");
-  if (is_null($return)) {
-    return drush_log("Failed to archive $source to $target", 'error');
-  }
+  execute_command("tar czf $target -C $source .");
 }
 
 function dump_database($uri, $drush_alias, $target) {
-  $command = "drush5 $drush_alias -l $uri sql-dump | gzip -9 > $target";
-  drush_print('Executing: ' . $command);
+  execute_command("drush5 $drush_alias -l $uri sql-dump | gzip -9 > $target");
+}
+
+function execute_command($command) {
+  drush_print("Executing: $command");
   $return = shell_exec($command);
   if (is_null($return)) {
-    return drush_log("Failed to dump database for $uri to $target", 'error');
+    return drush_log('Command execution failed. Processing has been aborted.', 'error');
   }
+  return $return;
 }
 
